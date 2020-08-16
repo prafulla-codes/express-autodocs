61ab7c8ccb9f7e6b9c2bd93e42b88d40a0d143da
Pika1998 <prafzzraichurkar1@gmail.com>
2020-08-16 19:20:16 +0530

    Changed state to original

--- index.js
+++ index.js
@@ -1,17 +1,14 @@
 const core = require("@actions/core");
 const github = require("@actions/github");
-const axios = require("axios");
+const fs = require("fs");
 try {
   // `who-to-greet` input defined in action metadata file
   console.log(core.get);
   const fileToScan = core.getInput("server-filename");
-  axios
-    .get(
-      `${github.context.payload.repository.html_url}/blob/master/${fileToScan}`
-    )
-    .then((data) => {
-      console.log(data);
-    });
+  fs.readFile(fileToScan, (err, data) => {
+    if (err) throw err;
+    console.log(data);
+  });
   const time = new Date().toTimeString();
   core.setOutput("time", time);
   // Get the JSON webhook payload for the event that triggered the workflow
