e26288765d6e5e2964ee468bca96e9042b480bff
Pika1998 <prafzzraichurkar1@gmail.com>
2020-08-16 14:30:43 +0530

    Installing axios

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
 const core = require("@actions/core");
 const github = require("@actions/github");
+const axios = require("axios");
 try {
   // `who-to-greet` input defined in action metadata file
   console.log(core.get);
   const fileToScan = core.getInput("server-filename");
-  fetch(
-    `${github.context.payload.repository.html_url}/blob/master/${fileToScan}`
-  ).then((data) => {
-    console.log(data);
-  });
+  axios
+    .get(
+      `${github.context.payload.repository.html_url}/blob/master/${fileToScan}`
+    )
+    .then((data) => {
+      console.log(data);
+    });
   const time = new Date().toTimeString();
   core.setOutput("time", time);
   // Get the JSON webhook payload for the event that triggered the workflow
