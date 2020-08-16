a8985c188324b82b5000590d5c844c7a8fbac289
Pika1998 <prafzzraichurkar1@gmail.com>
2020-08-16 13:37:05 +0530

    Initialized Basic Action

---
+++ index.js
@@ -0,0 +1,18 @@
+const core = require("@actions/core");
+const github = require("@actions/github");
+const fs = require("fs");
+try {
+  // `who-to-greet` input defined in action metadata file
+  const fileToScan = core.getInput("server-filename");
+  const file = fs.readFile(fileToScan, (err, data) => {
+    if (err) throw err;
+    console.log(data);
+  });
+  const time = new Date().toTimeString();
+  core.setOutput("time", time);
+  // Get the JSON webhook payload for the event that triggered the workflow
+  const payload = JSON.stringify(github.context.payload, undefined, 2);
+  console.log(`The event payload: ${payload}`);
+} catch (error) {
+  core.setFailed(error.message);
+}
