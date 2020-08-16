d7653cc946aa01a11d603add66dbc066c8022315
Pika1998 <prafzzraichurkar1@gmail.com>
2020-08-16 13:58:39 +0530

    changed url

--- index.js
+++ index.js
@@ -3,11 +3,15 @@ const github = require("@actions/github");
 const fs = require("fs");
 try {
   // `who-to-greet` input defined in action metadata file
+  console.log(core.get);
   const fileToScan = core.getInput("server-filename");
-  const file = fs.readFile(fileToScan, (err, data) => {
-    if (err) throw err;
-    console.log(data);
-  });
+  const file = fs.readFile(
+    `${github.context.payload.repository.blobs_url}`,
+    (err, data) => {
+      if (err) throw err;
+      console.log(data);
+    }
+  );
   const time = new Date().toTimeString();
   core.setOutput("time", time);
   // Get the JSON webhook payload for the event that triggered the workflow
