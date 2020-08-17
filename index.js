14a11b8bb4216cbe1f68433d5b61f3603f52869d
Pika1998 <prafzzraichurkar1@gmail.com>
2020-08-17 19:58:37 +0530

    Does not send payload anymore

--- index.js
+++ index.js
@@ -17,9 +17,6 @@ try {
   });
   const time = new Date().toTimeString();
   core.setOutput("time", time);
-  // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2);
-  console.log(`The event payload: ${payload}`);
 } catch (error) {
   core.setFailed(error.message);
 }
