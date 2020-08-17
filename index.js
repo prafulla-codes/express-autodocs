fdaf3dec81d6b2ef6756d91f40c9b26b893996c7
Pika1998 <prafzzraichurkar1@gmail.com>
2020-08-17 19:54:23 +0530

    added appname input parameter, tried basic regex

--- index.js
+++ index.js
@@ -2,12 +2,18 @@ const core = require("@actions/core");
 const github = require("@actions/github");
 const fs = require("fs");
 try {
-  // `who-to-greet` input defined in action metadata file
-  console.log(core.get);
+  // Get Backend Filename
   const fileToScan = core.getInput("server-filename");
+  // Get App Name
+  const appName = core.getInput("appname");
+  // * Step 1 - Reads Backend Server File
   fs.readFile(fileToScan, (err, data) => {
-    if (err) throw err;
-    console.log(data.toString("utf-8"));
+    if (err) throw err; // Throws error if unable to read backend file
+    backendFile = data.toString("utf-8");
+    let methodsRegexText = `${appName}\.(get|post|delete|patch|put).*`;
+    let methodsRegex = new RegExp(methodsRegexText, "g");
+    const scanMatches = backendFile.match(methodsRegex);
+    console.log(scanMatches);
   });
   const time = new Date().toTimeString();
   core.setOutput("time", time);
