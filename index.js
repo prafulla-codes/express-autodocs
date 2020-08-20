af12821e2209182721c9af30d6c5ce6ad0f170fc
Pika1998 <prafzzraichurkar1@gmail.com>
2020-08-20 15:48:10 +0530

    added params

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const core = require("@actions/core");
 const ExpressAutodocs = require("./ExpressAutodocs");
 
+process.env.NODE_ENV = "production";
 // If in production environment
 if (process.env.NODE_ENV == "production") {
-  const filePath = core.getInput();
-  ExpressAutodocs.generateDocs();
+  const filePath = core.getInput("server-filepath");
+  const appName = core.getInput("appname");
+  ExpressAutodocs.generateDocs(filePath, appName);
 } else {
   ExpressAutodocs.generateDocs("test/server.js", "app");
 }
