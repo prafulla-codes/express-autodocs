be40d67de77213fa7a3e3ab251ca894fa6979fdb
Hussain, Syed Muhammad Abid <syedmuhammadabid.h@alibaba-inc.com>
2020-10-01 14:55:08 +0500

    feat: ESLint and Prettier added

--- helpers/demestifyAPI.js
+++ helpers/demestifyAPI.js
@@ -1,9 +1,9 @@
-const vm = require("vm");
-function demestifyAPI(api, index, file, routeprefix = "") {
-  let METHOD = api.split(".")[1].split("(")[0];
+const vm = require('vm');
+function demestifyAPI(api, index, file, routeprefix = '') {
+  let METHOD = api.split('.')[1].split('(')[0];
   let trimmedCall = api.match(/\(.[\s\S]*/g)[0];
   trimmedCall = trimmedCall.substring(1, trimmedCall.length - 1);
-  let CALL = trimmedCall.split(",")[0];
+  let CALL = trimmedCall.split(',')[0];
   CALL = CALL.substring(1, CALL.length - 1);
   let params = getCustomParams(index, file);
   return {
@@ -15,17 +15,17 @@ function demestifyAPI(api, index, file, routeprefix = "") {
 
 function getCustomParams(index, file) {
   // Backtraces for an immedeate multi-line comment
-  let descriptionFound = "unknown";
+  let descriptionFound = 'unknown';
   let baseIndex = index;
   let currentIndex = baseIndex;
-  while (descriptionFound == "unknown") {
+  while (descriptionFound == 'unknown') {
     if (currentIndex != 0) {
       if (
-        file.charAt(currentIndex - 1) == " " ||
-        file.charAt(currentIndex - 1) == "\n"
+        file.charAt(currentIndex - 1) == ' ' ||
+        file.charAt(currentIndex - 1) == '\n'
       ) {
         currentIndex -= 1;
-      } else if (file.charAt(currentIndex - 1) == "/") {
+      } else if (file.charAt(currentIndex - 1) == '/') {
         descriptionFound = true;
       } else {
         descriptionFound = false;
@@ -43,20 +43,20 @@ function getCustomParams(index, file) {
 function getCommentedParams(index, file) {
   let startIndex = index - 3; //  will be the character before '*' character
   let currentIndex = startIndex;
-  let params = "";
-  while (file.charAt(currentIndex) != "*") {
+  let params = '';
+  while (file.charAt(currentIndex) != '*') {
     params = params + file.charAt(currentIndex);
     currentIndex -= 1;
   }
   try {
-    params = JSON.parse([...params].reverse().join("").trim());
+    params = JSON.parse([...params].reverse().join('').trim());
     return params;
   } catch (err) {
     console.log(
-      "\x1b[1m",
-      "\x1b[31m",
+      '\x1b[1m',
+      '\x1b[31m',
       `❌ Make sure the params passed in comments are in proper JSON Format.`,
-      "\x1b[0m"
+      '\x1b[0m'
     );
     return null;
   }
--- helpers/getCallsFromRoute.js
+++ helpers/getCallsFromRoute.js
@@ -1,10 +1,10 @@
-const vm = require("vm");
-const fs = require("fs");
-const path = require("path");
-const getFileFromRequire = require("./getFileHelpers/getFileFromRequire");
-const getFileFromVariableRoute = require("./getFileHelpers/getFileFromVariableRoute");
-const getFullCall = require("./getFullCall");
-const demestifyAPI = require("./demestifyAPI");
+const vm = require('vm');
+const fs = require('fs');
+const path = require('path');
+const getFileFromRequire = require('./getFileHelpers/getFileFromRequire');
+const getFileFromVariableRoute = require('./getFileHelpers/getFileFromVariableRoute');
+const getFullCall = require('./getFullCall');
+const demestifyAPI = require('./demestifyAPI');
 
 function getCallsFromRoute(route, basefile, basefilePath, routername) {
   // Gets The Route File
@@ -14,7 +14,7 @@ function getCallsFromRoute(route, basefile, basefilePath, routername) {
   let match,
     routeAPIMatchStartPositions = [];
   const routeLevelAPIRegexString = `${routername}.(get|post|delete|patch|put).*`;
-  const routeLevelAPIRegex = new RegExp(routeLevelAPIRegexString, "g");
+  const routeLevelAPIRegex = new RegExp(routeLevelAPIRegexString, 'g');
   let routeName = getRouteName(route);
   while ((match = routeLevelAPIRegex.exec(routeFile)))
     routeAPIMatchStartPositions.push(match.index);
@@ -27,18 +27,18 @@ function getCallsFromRoute(route, basefile, basefilePath, routername) {
 }
 
 function getRouteName(route) {
-  let routeName = route.split(",")[0].split("(")[1];
+  let routeName = route.split(',')[0].split('(')[1];
   routeName = routeName.substring(1, routeName.length - 1);
   return routeName;
 }
 
 function getRouteFile(route, basefile, filePath) {
-  let mainContent = route.split(".")[1].split("(")[0];
+  let mainContent = route.split('.')[1].split('(')[0];
   let trimmedRoute = route.match(/\(.[\s\S]*/g)[0];
   trimmedRoute = trimmedRoute.substring(1, trimmedRoute.length - 1);
-  let routeParams = [...trimmedRoute.split(",")];
+  let routeParams = [...trimmedRoute.split(',')];
   let lastParam = routeParams[routeParams.length - 1];
-  if (lastParam.includes("require")) {
+  if (lastParam.includes('require')) {
     let file = getFileFromRequire(lastParam.trim(), filePath);
     return file;
   } else {
