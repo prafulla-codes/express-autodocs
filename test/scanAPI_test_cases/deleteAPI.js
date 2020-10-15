ee596abf21daa746e81f692f9fc1ef69be62c2cd
Pika1998 <prafzzraichurkar1@gmail.com>
2020-10-15 11:45:24 +0530

    Added Scan API Tests

---
+++ test/scanAPI_test_cases/deleteAPI.js
@@ -0,0 +1,7 @@
+
+/**
+ * @param {string} description - Deletes An Item from database.
+ * @param {string} [inputs] itemid - The id of the item. */
+app.delete('/item', (req, res) => {
+    res.send(item);
+  });
---
+++ test/scanAPI_test_cases/putAPI.js
@@ -0,0 +1,8 @@
+
+/**
+ * @param {string} description - Puts an item to the database.
+ * @param {string} [inputs] name - The name of the item 
+ * */
+app.put('/item', (req, res) => {
+    res.send(item);
+  });
