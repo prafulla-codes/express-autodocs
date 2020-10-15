d8f3f935a2581339debff0432733345f48a990fd
Pika1998 <prafzzraichurkar1@gmail.com>
2020-10-15 11:45:24 +0530

    Added Scan API Tests

---
+++ test/scanAPI_test_cases/getAPI.js
@@ -0,0 +1,7 @@
+
+/**
+ * @param {string} description - Returns An Item from database.
+ * @param {string} [inputs] itemid - The id of the item. */
+app.get('/item', (req, res) => {
+  res.send(item);
+});
