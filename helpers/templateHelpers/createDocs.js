5452b28435998c269174f05a8c4408d979748b84
Pika1998 <prafzzraichurkar1@gmail.com>
2020-10-15 11:07:25 +0530

    Removed token occurences since it was unused

--- helpers/templateHelpers/createDocs.js
+++ helpers/templateHelpers/createDocs.js
@@ -1,13 +1,13 @@
 const generateMarkdowndocs = require('./markdown/generateMarkdownDocs');
 const generateStandardDocs = require('./standard/generateStandardDocs');
 
-function createDocs(apis, outputFormat, outputBranch, token, docsTitle) {
+function createDocs(apis, outputFormat, outputBranch, docsTitle) {
   switch (outputFormat) {
     case 'markdown':
-      generateMarkdowndocs(apis, outputBranch, token, docsTitle);
+      generateMarkdowndocs(apis, outputBranch, docsTitle);
       break;
     case 'standard':
-      generateStandardDocs(apis, outputBranch, token, docsTitle);
+      generateStandardDocs(apis, outputBranch, docsTitle);
       break;
     default:
       console.log('\x1b[1m', '\x1b[31m', `❌ Invalid Output Format`, '\x1b[0m');
